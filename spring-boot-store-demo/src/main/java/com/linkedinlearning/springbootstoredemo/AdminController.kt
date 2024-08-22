b47a0d7137a0c7e2ccda5e1f28d82e49cb7da388
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-08-22 10:32:39 -0400

    AdminController java to kotlin conversion and add @NotNull check to ProductService -> insert method

--- spring-boot-store-demo/src/main/java/com/linkedinlearning/springbootstoredemo/AdminController.kt
+++ spring-boot-store-demo/src/main/java/com/linkedinlearning/springbootstoredemo/AdminController.kt
@@ -1,45 +1,43 @@
-package com.linkedinlearning.springbootstoredemo;
+package com.linkedinlearning.springbootstoredemo
 
-import com.linkedinlearning.springbootstoredemo.model.Product;
-import com.linkedinlearning.springbootstoredemo.service.ProductService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.Map;
+import com.linkedinlearning.springbootstoredemo.model.Product
+import com.linkedinlearning.springbootstoredemo.service.ProductService
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.servlet.ModelAndView
+import java.util.Map
 
 /**
  * Controls the private admin portal for the site.
  * The admin area supports a minimal inventory view, and a form for adding new products
  */
 @Controller
-public class AdminController {
+class AdminController {
+    @Autowired
+    var productService: ProductService? = null
 
-    @Autowired ProductService productService;
-    private final Logger logger = LoggerFactory.getLogger(AdminController.class);
+    private val logger = LoggerFactory.getLogger(AdminController::class.java)
 
     @GetMapping("/admin")
-    public ModelAndView showProducts() {
-        var products = productService.allProducts();
-        return new ModelAndView("Admin", Map.of("products", products));
+    fun showProducts(): ModelAndView {
+        val products = productService!!.allProducts()
+        return ModelAndView("Admin", Map.of("products", products))
     }
 
     @GetMapping("/admin/new-product")
-    public ModelAndView newProduct() {
-        return new ModelAndView("ProductEntryForm");
+    fun newProduct(): ModelAndView {
+        return ModelAndView("ProductEntryForm")
     }
 
     @PostMapping("/admin/new-product")
-    public String formPost(Product product, Model model) {
-        var insertedProduct = productService.insert(product);
-        logger.info("/admin/new-product -> ${insertedProduct.name}");
-
-        model.addAttribute("product", insertedProduct);
-        return "ProductEntryForm";
+    fun formPost(product: Product?, model: Model): String {
+        val insertedProduct = productService!!.insert(product)
+        logger.info("/admin/new-product -> \${insertedProduct.name}")
+        model.addAttribute("product", insertedProduct)
+        return "ProductEntryForm"
     }
 }
