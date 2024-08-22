f567c9c219a073489408be4ebb1cfdbe296926f7
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-08-22 12:08:38 -0400

    Lot of conversion and refactoring

--- spring-boot-store-demo/src/main/java/com/linkedinlearning/springbootstoredemo/ui/converters/ProductToProductViewItemConverter.kt
+++ spring-boot-store-demo/src/main/java/com/linkedinlearning/springbootstoredemo/ui/converters/ProductToProductViewItemConverter.kt
@@ -1,15 +1,13 @@
-package com.linkedinlearning.springbootstoredemo.ui.converters;
+package com.linkedinlearning.springbootstoredemo.ui.converters
 
-import com.linkedinlearning.springbootstoredemo.model.Product;
-import com.linkedinlearning.springbootstoredemo.ui.ProductViewItem;
+import com.linkedinlearning.springbootstoredemo.model.Product
+import com.linkedinlearning.springbootstoredemo.ui.ProductViewItem
 
-public class ProductToProductViewItemConverter {
-    public static ProductViewItem productToProductViewItem(Product product) {
-        return new ProductViewItem(
-                product.getName(),
-                product.getDescription(),
-                product.getThumbnail(),
-                product.getType() + ":" + product.getUpc()
-        );
-    }
+fun Product.productToProductViewItem(): ProductViewItem {
+    return ProductViewItem(
+            name,
+            description,
+            thumbnail,
+            "$type:$upc"
+    )
 }
