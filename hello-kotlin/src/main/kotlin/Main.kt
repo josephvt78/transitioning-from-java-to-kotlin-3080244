d130896b5b3fc26393113d33238bc9300ad29b34
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-08-23 09:46:13 -0400

    assignment operators

--- hello-kotlin/src/main/kotlin/Main.kt
+++ hello-kotlin/src/main/kotlin/Main.kt
@@ -1,10 +1,14 @@
 fun main(args: Array<String>) {
-    var c = 30
-    var d = 40
-    println("c > d = "+(c>d))
-    println("c < d = "+(c.compareTo(d) < 0))
-    println("c >= d = "+(c>=d))
-    println("c <= d = "+(c.compareTo(d) <= 0))
-    println("c == d = "+(c==d))
-    println("c != d = "+(!(c?.equals(d) ?: (d === null))))
+    var a = 10
+    var b = 5
+    a+=b
+    println(a)
+    a-=b
+    println(a)
+    a*=b
+    println(a)
+    a/=b
+    println(a)
+    a%=b
+    println(a)
 }
