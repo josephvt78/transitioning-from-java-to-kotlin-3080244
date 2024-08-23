f2974330366ff9b7c8b4538c602925ee5233db32
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-08-23 07:00:55 -0400

    variable declaration is required

--- hello-kotlin/src/main/kotlin/Main.kt
+++ hello-kotlin/src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
 fun main(args: Array<String>) {
-    var alphabet: Char = 'C'
-    println("C is a character : ${alphabet is Char}")
+    var rollno = 260
+    var name = "joe"
+    name = "joe v"
+    println("name: ${name.uppercase()} & roll no: ${rollno}")
 }
