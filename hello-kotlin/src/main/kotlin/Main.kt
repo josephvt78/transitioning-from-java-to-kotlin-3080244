9d916a6e1c957ed8f399b4e51ba701f23f940629
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-08-23 11:27:19 -0400

    withIndex() usage for for loop

--- hello-kotlin/src/main/kotlin/Main.kt
+++ hello-kotlin/src/main/kotlin/Main.kt
@@ -1,29 +1,7 @@
-import java.util.Scanner
-
 fun main(args: Array<String>) {
+    var planets = arrayOf("Earth", "Mars", "Venus", "Jupiter", "Saturn")
 
-    // create an object for scanner class
-    val reader = Scanner(System.`in`)
-    print("Enter three numbers: ")
-
-    var num1 = reader.nextInt()
-    var num2 = reader.nextInt()
-    var num3 = reader.nextInt()
-
-    var max = if ( num1 > num2) {
-        if (num1 > num3) {
-            "$num1 is the largest number"
-        }
-        else {
-            "$num3 is the largest number"
-        }
+    for ((index,value) in planets.withIndex()) {
+        println("Element at $index th index is $value")
     }
-    else if( num2 > num3){
-        "$num2 is the largest number"
-    }
-    else{
-        "$num3 is the largest number"
-    }
-    println(max)
-
 }
