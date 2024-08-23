3ac53454d65d8a11b894d6ba074e2447580db50d
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-08-23 09:35:59 -0400

    arithmetic operators

--- hello-kotlin/src/main/kotlin/Main.kt
+++ hello-kotlin/src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
 fun main(args: Array<String>) {
-    val Rollno = 260
-    val name = "joe"
-    // name = "joe v" //results in compilation error
-    val name1 = name
-    println("name: ${name1.uppercase()} & roll no: ${Rollno}")
+    var a = 20
+    var b = 4
+    println("a + b = " + (a + b))
+    println("a - b = " + (a - b))
+    println("a * b = " + (a * (b)))
+    println("a / b = " + (a / b))
+    println("a % b = " + (a % (b)))
 }
