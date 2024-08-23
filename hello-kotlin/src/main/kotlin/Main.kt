966e828d83368a1766853f560a0cd8b4b39c194c
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-08-23 10:03:20 -0400

    read line from console print intern calls System.out.println() of java

--- hello-kotlin/src/main/kotlin/Main.kt
+++ hello-kotlin/src/main/kotlin/Main.kt
@@ -1,9 +1,5 @@
-fun main(args: Array<String>) {
-    println("5 signed shift left by 1 bit: " + 5.shl(1))
-    println("10 signed shift right by 2 bits: : " + 10.shr(2))
-    println("12 unsigned shift right by 2 bits:  " + 12.ushr(2))
-    println("36 bitwise and 22: " + 36.and(22))
-    println("36 bitwise or 22: " + 36.or(22))
-    println("36 bitwise xor 22: " + 36.xor(22))
-    println("14 bitwise inverse is: " + 14.inv())
+fun main(args : Array<String>) {
+    print("Enter text: ")
+    var input = readLine()
+    print("You entered: $input")
 }
