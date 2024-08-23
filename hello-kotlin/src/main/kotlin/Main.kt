26f6ab94f5578a2fae95182afd61a10a20bbd12a
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-08-23 06:50:36 -0400

    boolean data types

--- hello-kotlin/src/main/kotlin/Main.kt
+++ hello-kotlin/src/main/kotlin/Main.kt
@@ -1,15 +1,5 @@
 fun main(args: Array<String>) {
-    // add suffix F for float
-    var myfloat = 54F
-    println("My float value ${myfloat}")
-
-    var F1: Float = Float.MIN_VALUE
-    var F2: Float = Float.MAX_VALUE
-    println("Smallest Float value: " +F1)
-    println("Largest Float value: " + F2)
-
-    var D1: Double = Double.MIN_VALUE
-    var D2: Double = Double.MAX_VALUE
-    println("Smallest Double value: " + D1)
-    println("Largest Double value: " + D2)
+    if (true is Boolean){
+        print("Yes,true is a boolean value")
+    }
 }
