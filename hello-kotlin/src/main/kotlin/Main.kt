0545176fde84944cd5c63e035e5585bbcd9c82ec
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-08-23 06:48:30 -0400

    floating point number data types

--- hello-kotlin/src/main/kotlin/Main.kt
+++ hello-kotlin/src/main/kotlin/Main.kt
@@ -1,29 +1,15 @@
 fun main(args: Array<String>) {
-    var myint = 35
+    // add suffix F for float
+    var myfloat = 54F
+    println("My float value ${myfloat}")
 
-    // add suffix L for long integer
-    var mylong = 23L
+    var F1: Float = Float.MIN_VALUE
+    var F2: Float = Float.MAX_VALUE
+    println("Smallest Float value: " +F1)
+    println("Largest Float value: " + F2)
 
-    println("My integer ${myint}")
-    println("My long integer ${mylong}")
-
-    var b1: Byte = Byte.MIN_VALUE
-    var b2: Byte = Byte.MAX_VALUE
-    println("Smallest byte value: " +b1)
-    println("Largest byte value: " +b2)
-
-    var S1: Short = Short.MIN_VALUE
-    var S2: Short = Short.MAX_VALUE
-    println("Smallest short value: " +S1)
-    println("Largest short value: " +S2)
-
-    var I1: Int = Int.MIN_VALUE
-    var I2: Int = Int.MAX_VALUE
-    println("Smallest integer value: " +I1)
-    println("Largest integer value: " +I2)
-
-    var L1: Long = Long.MIN_VALUE
-    var L2: Long = Long.MAX_VALUE
-    println("Smallest long integer value: " +L1)
-    println("Largest long integer value: " +L2)
+    var D1: Double = Double.MIN_VALUE
+    var D2: Double = Double.MAX_VALUE
+    println("Smallest Double value: " + D1)
+    println("Largest Double value: " + D2)
 }
