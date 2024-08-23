8ee5af0e6cad7a8332a5ead9f33d1b04b3684eda
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-08-23 11:17:10 -0400

    quick way of defining array with values and looping and printing through it

--- hello-kotlin/src/main/kotlin/Main.kt
+++ hello-kotlin/src/main/kotlin/Main.kt
@@ -1,13 +1,6 @@
-fun main(args: Array<String>)
-{
-
-    println("259 to byte: " + (259.toByte()))
-    println("50000 to short: " + (50000.toShort()))
-    println("21474847499 to Int: " + (21474847499.toInt()))
-    println("10L to Int: " + (10L.toInt()))
-    println("22.54 to Int: " + (22.54.toInt()))
-    println("22 to float: " + (22.toFloat()))
-    println("65 to char: " + (65.toChar()))
-    // Char to Number is deprecated in kotlin
-    println("A to Int: " + ('A'.toInt()))
-}
+fun main(args: Array<String>) {			 //start of main block or outer block
+    val array = intArrayOf(2, 4, 6, 8)
+    for (element in array) {			 // start of inner block
+        println(element)
+    }									 // end of inner block
+}										 // end of main block
