2c5fa7eba85b1d150e77b8acb83d1da41a5246d2
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-08-23 10:12:20 -0400

    accepting numbers using readLine

--- hello-kotlin/src/main/kotlin/Main.kt
+++ hello-kotlin/src/main/kotlin/Main.kt
@@ -1,27 +1,16 @@
-import java.util.Scanner
-
 fun main(args: Array<String>) {
 
-    // create an object for scanner class
-    val number1 = Scanner(System.`in`)
-    print("Enter an integer: ")
-    // nextInt() method is used to take
-    // next integer value and store in enteredNumber1 variable
-    var enteredNumber1:Int = number1.nextInt()
-    println("You entered: $enteredNumber1")
+    print("Enter an Integer value: ")
+    val string1 = readLine()!!
 
-    val number2 = Scanner(System.`in`)
-    print("Enter a float value: ")
+    // .toInt() function converts the string into Integer
+    var integerValue: Int = string1.toInt()
+    println("You entered: $integerValue")
 
-    // nextFloat() method is used to take next
-    // Float value and store in enteredNumber2 variable
-    var enteredNumber2:Float = number2.nextFloat()
-    println("You entered: $enteredNumber2")
+    print("Enter a double value: ")
+    val string2= readLine()!!
 
-    val booleanValue = Scanner(System.`in`)
-    print("Enter a boolean: ")
-    // nextBoolean() method is used to take
-    // next boolean value and store in enteredBoolean variable
-    var enteredBoolean:Boolean = booleanValue.nextBoolean()
-    println("You entered: $enteredBoolean")
+    // .toDouble() function converts the string into Double
+    var doubleValue: Double = string2.toDouble()
+    println("You entered: $doubleValue")
 }
