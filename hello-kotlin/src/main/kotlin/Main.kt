e42b413d83283e6b41be966f52d2c8bec2f40680
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-08-23 14:18:16 -0400

    forEach in kotlin

--- hello-kotlin/src/main/kotlin/Main.kt
+++ hello-kotlin/src/main/kotlin/Main.kt
@@ -1,8 +1,5 @@
 fun main()
 {
     val arrayname = Array(5, { i -> i + 5 })
-    for (i in 0..arrayname.size-1)
-    {
-        println(arrayname[i])
-    }
+    arrayname.forEach({value -> println(value)})
 }
