c4cda2c176390ccfe679e577ae9f544d08bb676b
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-08-24 05:46:57 -0400

    function definition and calling with named arguments

--- hello-kotlin/src/main/kotlin/Main.kt
+++ hello-kotlin/src/main/kotlin/Main.kt
@@ -1,5 +1,11 @@
+fun student(name: String = "test name1", rollNo: Int = 0, grade: Int = 0) {
+    print("\n Name: $name, Roll No:$rollNo, Grade: $grade\n")
+}
+
 fun main()
 {
-    var s =" String"
-    print(s.subSequence(1, 4)) //Str
+    val n ="Joe"
+    val r = 123
+    val g = 12
+    student(rollNo=123, name="Ann")
 }
